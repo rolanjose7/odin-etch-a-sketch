e791c0dfd635b8dd1d7bcfd334428a415309b884
Rolan Jose <rolanjose7@hotmail.com>
2023-07-31 17:52:09 -0400

    Created hovering effects on squares, black for now

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-let container = document.querySelector('.container');
+const container = document.querySelector('.container');
+
 
 function createDiv(num){
     for(let i = 0; i < num; i++){
         var div1 = document.createElement('div');
         div1.classList.add('block');
         container.append(div1);
+        hoverDiv(div1);
     }
 }
 let numberOfDivs = 256;
 createDiv(numberOfDivs);
+
+function hoverDiv(div){
+    div.addEventListener('mouseover', (event)=>{div.classList.add('hovering')})
+}
+
+
+
