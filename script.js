586e93da4d17607bb406f8bb91d4fbdfd032545e
Rolan Jose <rolanjose7@hotmail.com>
2023-08-02 17:03:40 -0400

    Created the functionality of Color Picker, Clean and Eraser buttons

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 const container = document.querySelector('.container');
 
-
 function createDiv(num){
     for(let i = 0; i < num; i++){
         var div1 = document.createElement('div');
         div1.classList.add('block');
-        container.append(div1);
         hoverDiv(div1);
+        eraseDiv(div1);
+        container.append(div1);
     }
 }
 let numberOfDivs = 256;
 createDiv(numberOfDivs);
 
 function hoverDiv(div){
-    div.addEventListener('mouseover', (event)=>{div.classList.add('hovering')})
+    const colorPick = document.getElementById('colorpicker');
+    div.addEventListener('mouseover', function(){
+        div.style.backgroundColor = colorPick.value;
+
+    div.addEventListener('mouseover', (event)=>{div.style.backgroundColor = colorPick.value})
+    })
+}
+function eraseDiv(div){
+    const eraserBtn = document.getElementById('eraser');
+    eraserBtn.addEventListener('click', function(){
+        div.addEventListener('mouseover', function(){
+            div.style.backgroundColor = 'white';
+        })
+    })
 }
 
+const cleanBtn = document.getElementById('clean');
+cleanBtn.addEventListener('click',(event)=> {location.reload()});
 
 
